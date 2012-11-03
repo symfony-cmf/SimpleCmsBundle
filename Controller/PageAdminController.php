cf759063c63f2af9cb8cadc98c7956f748ddea9d
Pawel Martuszewski <contact@pawelmartuszewski.co.uk>
2012-11-03 21:34:36 +0000

    Removed unwanted leftover 'use' throwing fatal 2.

--- Controller/PageAdminController.php
+++ Controller/PageAdminController.php
@@ -2,8 +2,6 @@
 
 namespace Symfony\Cmf\Bundle\SimpleCmsBundle\Controller;
 
-use Sonata\AdminBundle\Controller\CRUDController as Controller;
-
-class PageAdminController extends Controller
+class PageAdminController
 {
 }
